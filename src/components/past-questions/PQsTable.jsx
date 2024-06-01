8520958f30f058f6ba8ea14b83ebb683e1a9ebbe
hokageCodes <hashsenju94@gmail.com>
2024-06-01 13:59:04 +0100

    final push

--- src/components/past-questions/PQsTable.jsx
+++ src/components/past-questions/PQsTable.jsx
@@ -114,12 +114,8 @@ function PQsTable() {
                 <td className="border border-gray-400 px-4 py-2 text-center">{question.year}</td>
                 <td className="border border-gray-400 px-4 py-2 text-center">{question.semester}</td>
                 <td className="border border-gray-400 px-4 py-2 text-center">
-                  <a href={question.fileUrl} download={question.fileName} className="text-blue-500 font-semibold hover:underline flex items-center justify-center">
+                  <a href={question.fileUrl} download className="text-blue-500 font-semibold hover:underline flex items-center justify-center">
                     <HiOutlineDocumentDownload className="mr-1" /> Download
-                  </a>
-
-                  <a href={question.fileUrl} target="_blank" rel="noopener noreferrer" className="text-blue-500 font-semibold hover:underline flex items-center justify-center mt-2">
-                    <HiOutlineDocumentDownload className="mr-1" /> View
                   </a>
                 </td>
               </tr>
